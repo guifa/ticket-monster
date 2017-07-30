62031357a0a65820ba87aff36436397c6fe10744
guifa <guifa@guifa-TWS>
2017-07-30 02:35:29 -0300

    Initial commit of TicketMonster

---
+++ ticket-monster/src/main/java/org/jboss/examples/ticketmonster/model/Event.java
@@ -0,0 +1,109 @@
+package org.jboss.examples.ticketmonster.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Event implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+
+	@Column(unique = true)
+	@NotNull
+	@Size(min = 5, max = 50, message = "An event's name must contain between 5 and 50 characters")
+	private String name;
+
+	@Column
+	@Size(min = 20, max = 1000, message = "An event's description must contain between 20 and 100 characters")
+	private String description;
+	
+	@ManyToOne
+	private MediaItem mediaItem;
+	
+	@ManyToOne
+	@NotNull
+	private EventCategory eventCategory;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public MediaItem getMediaItem() {
+		return mediaItem;
+	}
+
+	public void setMediaItem(MediaItem mediaItem) {
+		this.mediaItem = mediaItem;
+	}
+	
+	public EventCategory getEventCategory() {
+		return eventCategory;
+	}
+
+	public void setEventCategory(EventCategory eventCategory) {
+		this.eventCategory = eventCategory;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Event other = (Event) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
