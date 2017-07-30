453343cea5c539b1a96b6541497ba61e275bcdbe
guifa <guifa@guifa-TWS>
2017-07-30 02:52:00 -0300

    Add Show model

---
+++ ticket-monster/src/main/java/org/jboss/examples/ticketmonster/model/Show.java
@@ -0,0 +1,87 @@
+package org.jboss.examples.ticketmonster.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Show {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@NotNull
+	private Event event;
+	
+	@ManyToOne
+	@NotNull
+	private Venue venue;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Event getEvent() {
+		return event;
+	}
+
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+
+	public Venue getVenue() {
+		return venue;
+	}
+
+	public void setVenue(Venue venue) {
+		this.venue = venue;
+	}
+
+	@Override
+	public String toString() {
+		return "Show [event=" + event + ", venue=" + venue + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((event == null) ? 0 : event.hashCode());
+		result = prime * result + ((venue == null) ? 0 : venue.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Show other = (Show) obj;
+		if (event == null) {
+			if (other.event != null)
+				return false;
+		} else if (!event.equals(other.event))
+			return false;
+		if (venue == null) {
+			if (other.venue != null)
+				return false;
+		} else if (!venue.equals(other.venue))
+			return false;
+		return true;
+	}
+	
+	
+
+}
